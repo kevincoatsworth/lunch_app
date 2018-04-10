5005952ad9ab67f66378c23ec3a89fdb2678f82c
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-04-10 13:23:24 +0100

    Implement Java calendar for time based greeting.

--- app/services/GreetingService.scala
+++ app/services/GreetingService.scala
@@ -1,9 +1,18 @@
 package services
 
+import java.util.Calendar
+
 import com.google.inject.ImplementedBy
 
 class RealGreetingService extends GreetingService {
-  def greeting: String = "Alright!"
+  def greeting: String = {
+    val now = Calendar.getInstance()
+    val currentHour = now.get(Calendar.HOUR_OF_DAY)
+    if (currentHour < 12)
+      "Good morning!"
+    else
+      "Good afternoon!"
+  }
 }
 
 @ImplementedBy(classOf[RealGreetingService])
